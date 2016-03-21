8dc3f18ffedd026984a297c29b89b4acfb8fe9b6
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-03-21 22:41:32 +0000

    Refacted out the OpenCL bits into a separate class

--- FractalsWpf/MandelbrotSetGpu.cs
+++ FractalsWpf/MandelbrotSetGpu.cs
@@ -1,31 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Numerics;
-using System.Reflection;
 using OpenCL;
 
 namespace FractalsWpf
 {
     class MandelbrotSetGpu : IFractals, IDisposable
     {
-        private readonly OpenCLContext _context;
-        private readonly OpenCLCommandQueue _commandQueue;
-        private readonly OpenCLKernel _kernel;
+        private readonly OpenCLRunner _runner;
 
         public MandelbrotSetGpu()
         {
-            var platform = OpenCLPlatform.Platforms.FirstOrDefault(p => p.Vendor.Contains("NVIDIA"));
-            _context = new OpenCLContext(
-                OpenCLDeviceType.Gpu,
-                new OpenCLContextPropertyList(platform),
-                null,
-                IntPtr.Zero);
-            var device = _context.Devices.First();
-            _commandQueue = new OpenCLCommandQueue(_context, device, OpenCLCommandQueueProperties.None);
-            var program = LoadProgram(_context, device, "FractalsWpf.CreateMandelbrotSetPixelArray.cl");
-            _kernel = program.CreateKernel("CreateMandelbrotSetPixelArray");
+            _runner = new OpenCLRunner("FractalsWpf.CreateMandelbrotSetPixelArray.cl");
         }
 
         public int[] CreatePixelArray(
@@ -42,32 +27,30 @@ namespace FractalsWpf
             var maxImaginary = (float)Math.Max(c1.Imaginary, c2.Imaginary);
             var realDelta = (maxReal - minReal)/(numWidthDivisions - 1);
             var imaginaryDelta = (maxImaginary - minImaginary)/(numHeightDivisions - 1);
-
             var numResults = numWidthDivisions * numHeightDivisions;
             var results = new int[numResults];
-
             const OpenCLMemoryFlags bufferFlags = OpenCLMemoryFlags.WriteOnly | OpenCLMemoryFlags.AllocateHostPointer;
             var bufferCount = new long[] {numResults};
             var bufferElementType = typeof (int);
 
-            using (var resultsBuffer = new OpenCLBuffer(_context, bufferFlags, bufferElementType, bufferCount))
+            using (var resultsBuffer = new OpenCLBuffer(_runner.Context, bufferFlags, bufferElementType, bufferCount))
             {
-                _kernel.SetValueArgument(0, minReal);
-                _kernel.SetValueArgument(1, minImaginary);
-                _kernel.SetValueArgument(2, realDelta);
-                _kernel.SetValueArgument(3, imaginaryDelta);
-                _kernel.SetValueArgument(4, maxIterations);
-                _kernel.SetMemoryArgument(5, resultsBuffer);
+                _runner.Kernel.SetValueArgument(0, minReal);
+                _runner.Kernel.SetValueArgument(1, minImaginary);
+                _runner.Kernel.SetValueArgument(2, realDelta);
+                _runner.Kernel.SetValueArgument(3, imaginaryDelta);
+                _runner.Kernel.SetValueArgument(4, maxIterations);
+                _runner.Kernel.SetMemoryArgument(5, resultsBuffer);
 
                 var globalWorkSize = new long[] { numHeightDivisions, numWidthDivisions };
-                _commandQueue.Execute(_kernel, null, globalWorkSize, null);
+                _runner.CommandQueue.Execute(_runner.Kernel, null, globalWorkSize, null);
 
                 using (var resultsHandle = new PinnedObject(results))
                 {
-                    _commandQueue.ReadFromBuffer(resultsBuffer, resultsHandle, true, 0L, numResults);
+                    _runner.CommandQueue.ReadFromBuffer(resultsBuffer, resultsHandle, true, 0L, numResults);
                 }
 
-                _commandQueue.Finish();
+                _runner.CommandQueue.Finish();
 
                 return results;
             }
@@ -75,37 +58,7 @@ namespace FractalsWpf
 
         public void Dispose()
         {
-            _kernel?.Dispose();
-            _commandQueue?.Dispose();
-            _context?.Dispose();
-        }
-
-        private static OpenCLProgram LoadProgram(OpenCLContext context, OpenCLDevice device, string resourceName)
-        {
-            var source = GetProgramSourceFromResource(Assembly.GetExecutingAssembly(), resourceName);
-            var program = new OpenCLProgram(context, source);
-
-            try
-            {
-                program.Build(new List<OpenCLDevice> { device }, string.Empty, null, IntPtr.Zero);
-            }
-            catch (BuildProgramFailureOpenCLException)
-            {
-                var buildLog = program.GetBuildLog(device);
-                throw new ApplicationException($"Error building program \"{resourceName}\":{Environment.NewLine}{buildLog}");
-            }
-
-            return program;
-        }
-
-        private static string GetProgramSourceFromResource(Assembly assembly, string resourceName)
-        {
-            using (var stream = assembly.GetManifestResourceStream(resourceName))
-            {
-                if (stream == null) throw new ApplicationException($"Failed to load resource {resourceName}");
-                var streamReader = new StreamReader(stream);
-                return streamReader.ReadToEnd();
-            }
+            _runner.Dispose();
         }
     }
 }
---
+++ FractalsWpf/OpenCLRunner.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using OpenCL;
+
+namespace FractalsWpf
+{
+    // ReSharper disable once InconsistentNaming
+    public class OpenCLRunner : IDisposable
+    {
+        public OpenCLRunner(string resourceName)
+        {
+            var platform = OpenCLPlatform.Platforms.FirstOrDefault(p => p.Vendor.Contains("NVIDIA"));
+            Context = new OpenCLContext(
+                OpenCLDeviceType.Gpu,
+                new OpenCLContextPropertyList(platform),
+                null,
+                IntPtr.Zero);
+            var device = Context.Devices.First();
+            CommandQueue = new OpenCLCommandQueue(Context, device, OpenCLCommandQueueProperties.None);
+            var program = LoadProgram(resourceName);
+            Kernel = program.CreateKernel("CreateMandelbrotSetPixelArray");
+        }
+
+        public OpenCLContext Context { get; }
+        public OpenCLCommandQueue CommandQueue { get; }
+        public OpenCLKernel Kernel { get; }
+
+        public void Dispose()
+        {
+            Kernel?.Dispose();
+            CommandQueue?.Dispose();
+            Context?.Dispose();
+        }
+
+        private OpenCLProgram LoadProgram(string resourceName)
+        {
+            var source = GetProgramSourceFromResource(Assembly.GetExecutingAssembly(), resourceName);
+            var program = new OpenCLProgram(Context, source);
+
+            try
+            {
+                program.Build(new List<OpenCLDevice> {CommandQueue.Device}, string.Empty, null, IntPtr.Zero);
+            }
+            catch (BuildProgramFailureOpenCLException)
+            {
+                var buildLog = program.GetBuildLog(CommandQueue.Device);
+                throw new ApplicationException($"Error building program \"{resourceName}\":{Environment.NewLine}{buildLog}");
+            }
+
+            return program;
+        }
+
+        private static string GetProgramSourceFromResource(Assembly assembly, string resourceName)
+        {
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null) throw new ApplicationException($"Failed to load resource {resourceName}");
+                var streamReader = new StreamReader(stream);
+                return streamReader.ReadToEnd();
+            }
+        }
+    }
+}
